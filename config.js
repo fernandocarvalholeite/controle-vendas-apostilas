3d472dfec78651b13d006af81ccaf623dd26179d
fernandocarvalholeite <fcconsultoria25@hotmail.com>
2026-08-14 21:10:27 -0400

    Add Supabase configuration and email constants

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
+const SUPABASE_URL = 'https://cpkjphodruyxfouirhpd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwa2pwaG9kcnV5eGZvdWlyaHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3Mzg3NTQsImV4cCI6MjEwMjMxNDc1NH0.5OAmhWSe_4STLaCo-NfAwxGDRCGe3ICk-j5IEAMStKQ';
 
+const EMAIL_FC = 'fc.consultoria@apostilas-ricardo.app';
+const EMAIL_RICARDO = 'ricardo.camara@apostilas-ricardo.app';
